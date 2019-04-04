3ba1543a220f3e903088f8cb14ce74dc30cbe994
marinoalan <marinoalanunq@gmail.com>
2019-04-04 00:23:00 -0300

    a

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/grupo6-juegos-yop-frontend'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile('/dist/index.html');
+    res.sendFile('index.html');
 });
 
 // Start the app by listening on the default Heroku port
