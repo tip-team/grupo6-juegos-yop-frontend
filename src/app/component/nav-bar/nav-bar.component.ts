90924e3506053b177efaaa1c574374aff3ae8af4
marinoalan <marinoalanunq@gmail.com>
2019-05-28 00:42:46 -0300

    Prueba position px.

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -47,7 +47,8 @@ export class NavBarComponent {
   stylePosition;
   
   constructor(private router: Router, private activatedRoute: ActivatedRoute, public authService: AuthService, private deviceService: DeviceDetectorService) {
-    this.stylePosition = {'position': 'absolute', 'bottom': this.deviceService.isMobile() ? '35' : '0' + 'px'};
+    const bottomPosition = this.deviceService.isMobile() ? '63' : '0';
+    this.stylePosition = {'position': 'absolute', 'bottom': bottomPosition + 'px'};
     const baseUrl = '/';
     if (this.router.url === baseUrl) {
       this.router.navigateByUrl(baseUrl + this.app.INVENTARIO.path);
