98f89a92656e849884b08840ce8f468eed5e7469
marinoalan <marinoalanunq@gmail.com>
2019-06-18 00:04:10 -0300

    Se cambio el contenido de la página crecer jugando.

--- src/app/component/crecer-jugando/crecer-jugando.ts
+++ src/app/component/crecer-jugando/crecer-jugando.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
     selector: 'crecer-jugando',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./crecer-jugando.css']
 })
 export class CrecerJugandoComponent {
+
+    textSize;
+    imagenHeight;
+    imagenMargin;
+
+    constructor(private deviceService: DeviceDetectorService) {
+        const isMobile = this.deviceService.isMobile();
+        const fontSizeTitle = isMobile ? '18' : '25.7';
+        this.textSize = { 'font-size': fontSizeTitle + 'px' };
+        const imagenHeighResponsive = isMobile ? '80' : '90';
+        this.imagenHeight = { 'max-height': imagenHeighResponsive + 'vh' };
+        this.imagenMargin = isMobile ? { 'margin-top': '15px', 'margin-bottom': '20px' } : {};
+    }
+
 }
