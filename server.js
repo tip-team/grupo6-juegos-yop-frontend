702353e106ee4a04caabd7112e1340271d361bf6
marinoalan <marinoalanunq@gmail.com>
2019-06-18 19:24:30 -0300

    Se modifico server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(compression());
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist/grupo6-juegos-yop-frontend'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(__dirname + 'index.html');
+    res.sendFile(path.join(__dirname+'/dist/grupo6-juegos-yop-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
