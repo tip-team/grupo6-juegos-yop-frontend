d8aca6c339dd285e342a17861e322984ef60df3e
marinoalan <marinoalanunq@gmail.com>
2019-06-18 18:35:27 -0300

    Se quito el hash en el path de la url.

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(compression());
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
