fccd8ca476e17166b8b40125e8b5ca17346489df
Ruben Novelli <rubendario.novel@gmail.com>
2019-06-28 09:10:48 -0300

    GC

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const path = require('path');
 const app = express();
 app.use(compression());
 
+var oneYear = 60 * 1000 * 60 * 24 * 365;
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist', { maxAge: oneYear }));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
